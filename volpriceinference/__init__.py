12855b9f3906639783782cad97aaa9276406181e
sangrey <paul.sangrey@gmail.com>
2018-08-14 17:02:20 -0400

    Fixed another typo.

--- volpriceinference/__init__.py
+++ volpriceinference/__init__.py
@@ -1,4 +1,4 @@
-from libvolpriceinference import _simulate_autoregressive_gamma, _threadsave_gaussian_rvs
+from libvolpriceinference import _simulate_autoregressive_gamma, _threadsafe_gaussian_rvs
 from .version import __version__
 from .volprice import *
 
