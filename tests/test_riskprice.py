Fixed one bug and exporting one of the partial derivatives.

tests/test_riskprice.py
@@ -17,11 +17,10 @@ phi_floats = st.floats(min_value=-.9, max_value=-.1)
 def test_A_func(logit_rho, log_scale, x):
     """ Ensure the two A_func implementations return the same value. """
     import volpriceinference as vl
-    import libvolpriceinference as lvl
     
     val1 = sym.N(vl.volprice._A_func.replace(vl.logit_rho, logit_rho).replace(
         vl.log_scale, log_scale).replace(vl.volprice._x, x))
-    val2 = lvl.A_func(logit_rho=logit_rho, log_scale=log_scale, x=x)
+    val2 = lv.A_func(logit_rho=logit_rho, log_scale=log_scale, x=x)
     
     if np.isfinite(val2):
         assert np.isclose(float(np.real(val1)), float(np.real(val2)), equal_nan=True, rtol=1e-3),\
@@ -32,11 +31,10 @@ def test_A_func(logit_rho, log_scale, x):
 def test_B_func(log_both, log_scale, x):
     """ Ensure the two B_func implementations return the same value. """
     import volpriceinference as vl
-    import libvolpriceinference as lvl
     
     val1 = sym.N(sym.exp(log_both - log_scale) * (sym.log(vl.volprice._B_func_in.replace(
         vl.volprice._x, x).replace(vl.log_scale, log_scale))))
-    val2 = lvl.B_func(log_both=log_both, log_scale=log_scale, x=x)
+    val2 = lv.B_func(log_both=log_both, log_scale=log_scale, x=x)
 
     if np.isfinite(val2):
         assert np.isclose(float(np.real(val1)), float(np.real(val2)), equal_nan=True, rtol=1e-3),\
@@ -47,11 +45,10 @@ def test_B_func(log_both, log_scale, x):
 def test_link2(pi, theta, log_both, log_scale, phi, psi):
     """ Ensure the two implmentation fo the link2 function return the same value. """
     import volpriceinference as vl
-    import libvolpriceinference as lvl
     
     val1 = sym.N(vl.volprice._gamma_sym.replace(vl.theta, theta).replace(vl.pi, pi).replace(
         vl.log_both, log_both)).replace(vl.log_scale, log_scale).replace(vl.phi, phi).replace(vl.psi, psi)
-    val2 = lvl.link2(pi=pi, theta=theta, log_both=log_both, log_scale=log_scale, phi=phi, psi=psi)
+    val2 = lv.link2(pi=pi, theta=theta, log_both=log_both, log_scale=log_scale, phi=phi, psi=psi)
 
     if np.isfinite(val2):
         assert np.isclose(float(np.real(val1)), float(np.real(val2)), equal_nan=True, rtol=1e-3),\